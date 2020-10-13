6870ea487a38f6a6582a0f30905b25e944e818e9
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-10-13 23:21:37 +0530

    Create guessTheGame.java

---
+++ Number Game/guessTheGame.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.lang.*;
+
+public class NumberGame {
+    static{
+        System.out.println("RULES:-");
+        System.out.println("1.Should be a three digit number.");
+        System.out.println("2.Time limit is limited to 60 seconds.");
+        System.out.println("3.Person with least number of guesses wins.");
+        System.out.println("4.Maximum guesses that would be provided will be 30");
+        System.out.println("5.HAPPY GUESSING... ;)");
+    }
+
+    public static boolean guessTheGame(int guess,int[] arr) {
+        int guessRem, i = 2;
+        int[] guessArray = new int[3];
+        boolean numberPresent=false,runAgain=true;
+        while (guess > 0) {
+            guessRem = guess % 10;
+            guessArray[i] = guessRem;
+            i--;
+            guess = guess / 10;
+        }
+        if (Arrays.equals(guessArray, arr)) {
+            System.out.println("Yeeeeeeeeeeee...You Won!");
+            runAgain= false;
+        }
+        else {
+            if(guessArray[0]==arr[0] || guessArray[1]==arr[1] || guessArray[2]==arr[2])
+                System.out.println("MATCH");
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (guessArray[j] == arr[k]) {
+                        numberPresent = true;
+                        break;
+                    }
+                }
+            }
+            if(numberPresent){
+                System.out.println("CLOSE");
+            }
+            else{
+            System.out.println("NONE");
+            }
+        }
+        return runAgain;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner((System.in));
+        int min = 100, max = 999, i = 2, rem, count = 0;
+        int[] arr = new int[3];
+        boolean guess = true;
+        int number = (int) Math.floor(Math.random() * (max - min) + min);
+        System.out.println(number);
+        while (number > 0) {
+            rem = number % 10;
+            arr[i] = rem;
+            i--;
+            number = number / 10;
+        }
+        while (guess) {
+            long startTime=System.currentTimeMillis();
+            if (count <= 30) {
+                System.out.println("Enter Your Guess...");
+                count++;
+                int guessNumber = scan.nextInt();
+                long endTime=System.currentTimeMillis();
+                if (guessNumber >= 100 && guessNumber <= 999)
+                    if((endTime-startTime)<=60000)
+                        guess = guessTheGame(guessNumber, arr);
+                    else {
+                        System.out.println("Time out...Please read above rules.");
+                        guess=false;
+                    }
+                else
+                    System.out.println("Please Enter a three digit number.");
+            }
+        }
+        System.out.println("the number of guesses made: " + count);
+    }
+}
