e1473f966bb8355ee008c513ee21da1e892f94a0
Adnan Anjum <64261344+adnan3856@users.noreply.github.com>
2020-10-14 21:40:45 +0530

    Update guessTheGame.java

--- Number Game/guessTheGame.java
+++ Number Game/guessTheGame.java
@@ -1,4 +1,52 @@
-public static void main(String[] args) {
+import java.util.*;
+import java.lang.*;
+
+public class NumberGame {
+    static{
+        System.out.println("RULES:-");
+        System.out.println("1.Should be a three digit number.");
+        System.out.println("2.Time limit is limited to 60 seconds.");
+        System.out.println("3.Person with least number of guesses wins.");
+        System.out.println("4.Maximum guesses that would be provided will be 30");
+        System.out.println("5.HAPPY GUESSING... ;)");
+    }
+    
+    public static boolean guessTheGame(int guess,int[] arr,int num) {
+        int guessRem, i = 2;
+        int[] guessArray = new int[3];
+        boolean numberPresent=false,runAgain=true;
+        while (guess > 0) {
+            guessRem = guess % 10;
+            guessArray[i] = guessRem;
+            i--;
+            guess = guess / 10;
+        }
+        if (Arrays.equals(guessArray, arr)) {
+            System.out.println("Yeeeeeeeeeeee...You Won!. The number was: "+num);
+            runAgain= false;
+        }
+        else {
+            if(guessArray[0]==arr[0] || guessArray[1]==arr[1] || guessArray[2]==arr[2])
+                System.out.println("MATCH");
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (guessArray[j] == arr[k]) {
+                        numberPresent = true;
+                        break;
+                    }
+                }
+            }
+            if(numberPresent){
+                System.out.println("CLOSE");
+            }
+            else{
+            System.out.println("NONE");
+            }
+        }
+        return runAgain;
+    }
+    
+    public static void main(String[] args) {
         Scanner scan = new Scanner((System.in));
         int min = 100, max = 999, i = 2, rem, count = 0;
         int[] arr = new int[3];
@@ -37,3 +85,4 @@ public static void main(String[] args) {
         }
         System.out.println("the number of guesses made: " + count);
     }
+}
